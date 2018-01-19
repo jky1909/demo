7ef2094f46b55c671fd619b12fa7b2378ffe6c05
jky <jky1909@163.com>
2018-01-19 16:24:31 +0800

    原型模式

---
+++ src/com/jky/design/patterns/prototype/Prototype.java
@@ -0,0 +1,59 @@
+package com.jky.design.patterns.prototype;
+
+import java.io.*;
+
+/**
+ * Create by jky on 2018/01/19
+ * 原型模式
+ * 浅复制：将一个对象复制后，基本数据类型的变量都会重新创建，而引用类型，指向的还是原对象所指向的。
+ 深复制：将一个对象复制后，不论是基本数据类型还有引用类型，都是重新创建的。简单来说，就是深复制进行了完全彻底的复制，而浅复制不彻底。
+ */
+public class Prototype implements Cloneable,Serializable {
+
+    private static final long serialVersionUID = -4172091113908315286L;
+
+    private String string;
+
+    private SerializableObject  obj;
+
+    /**
+     * 浅复制
+     * @return
+     */
+    public Object clone() throws CloneNotSupportedException {
+        Prototype prototype = (Prototype) super.clone();
+        return prototype;
+    }
+
+    /**
+     * 深复制
+     * @return
+     */
+    public Object deepClone() throws IOException, ClassNotFoundException {
+        /* 写入当前对象的二进制流 */
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(this);
+
+        /* 读出二进制流产生的新对象 */
+        ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        return ois.readObject();
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public SerializableObject getObj() {
+        return obj;
+    }
+
+    public void setObj(SerializableObject obj) {
+        this.obj = obj;
+    }
+}
---
+++ src/com/jky/design/patterns/prototype/SerializableObject.java
@@ -0,0 +1,8 @@
+package com.jky.design.patterns.prototype;
+
+import java.io.Serializable;
+
+public class SerializableObject implements Serializable {
+
+    private static final long serialVersionUID = 8447064560857497583L;
+}
