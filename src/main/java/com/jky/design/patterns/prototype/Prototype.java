ff1b930b978da3715ee27dc18445ef3ac10e739f
jky <jky1909@163.com>
2018-01-26 11:42:33 +0800

    增加maven

--- src/main/java/com/jky/design/patterns/prototype/Prototype.java
+++ src/main/java/com/jky/design/patterns/prototype/Prototype.java
@@ -3,7 +3,7 @@ package com.jky.design.patterns.prototype;
 import java.io.*;
 
 /**
- * Create by jky on 2018/01/19
+ * @author jky
  * 原型模式
  * 浅复制：将一个对象复制后，基本数据类型的变量都会重新创建，而引用类型，指向的还是原对象所指向的。
  深复制：将一个对象复制后，不论是基本数据类型还有引用类型，都是重新创建的。简单来说，就是深复制进行了完全彻底的复制，而浅复制不彻底。
--- src/main/java/com/jky/staticdemo/Main.java
+++ src/main/java/com/jky/staticdemo/Main.java
@@ -1,5 +1,11 @@
 package com.jky.staticdemo;
 
+/**
+ * @author jky
+ * @date 2018/01/24
+ *
+ * java 类加载的顺序
+ */
 public class Main {
 
     public static void main(String[] args) {
